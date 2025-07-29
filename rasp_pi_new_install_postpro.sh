92a6a82e1d61fa3dce44e81f5751df9db1b1525a
Pawel <zzrock@outlook.com>
2025-07-29 17:51:21 +0200

    Update rasp_pi_new_install_postpro.sh

--- rasp_pi_new_install_postpro.sh
+++ rasp_pi_new_install_postpro.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Git configuration"
+    git config --global user.name "Name"
+    git config --global user.email "email@test.com"
+    echo "Generating an SSH key"
+    ssh-keygen -o -t rsa -C “email@test.com”
+    echo "Configuration complete, run the script again as root."
+    exit 1
+else
+    echo "Running script as root..."
+    echo "apt update"
+    apt update
+
+    echo "apt upgrade"
+    apt upgrade -y
+
+    echo "apt install mc nmon"
+    apt install mc nmon -y
+
+    echo "Script execution completed successfully."
+    echo "Execute the 'reboot' command to restart the system."
+    exit 0
+fi
