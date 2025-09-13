163e743b7df92655128e19b36b31db99a1be4bc2
Pawel <zzrock@outlook.com>
2025-09-13 22:42:51 +0200

    modified:   robocopy.bat

--- Archive/robocopy.bat
+++ Archive/robocopy.bat
@@ -4,5 +4,4 @@ REM Description: Synchronizes the contents of E:\Photos_Videos to F:\Photos_Vide
 REM              using robocopy with mirror mode. Retries twice on failure, waits
 REM              5 seconds between retries, and logs output to E:\Logs\Photos_Videos.log.
 REM -----------------------------------------------------------------------------
-robocopy E:\Photos_Videos F:\Photos_Videos /MIR
 robocopy E:\Photos_Videos F:\Photos_Videos /MIR /R:2 /W:5 /LOG:E:\Logs\Photos_Videos.log
